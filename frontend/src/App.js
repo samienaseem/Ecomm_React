ef97b4870723d2320f3788599ee3da92316d4390
Saquib Naseem <samar.naseem.97@gmail.com>
2024-10-02 05:55:28 +0100

    Bootstrap added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,6 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import { LinkContainer } from 'react-router-bootstrap';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomeScreen from './Screens/HomeScreen';
@@ -8,15 +11,23 @@ function App() {
     <BrowserRouter>
       <div>
         <header>
-          <a href="/">11th Street Atelier</a>
+          <Navbar bg="dark" variant="dark">
+            <Container fluid>
+              <LinkContainer to="/">
+                <Navbar.Brand>11th Street Atelier</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+          {/* <a href="/">11th Street Atelier</a> */}
         </header>
 
         <main>
-          <Routes>
-            <Route path='/product/:slug' element={<ProductScreen />}/>
-            <Route path='/' element={<HomeScreen />}/>
-          </Routes>
-          
+          <Container fluid>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
         </main>
       </div>
     </BrowserRouter>
