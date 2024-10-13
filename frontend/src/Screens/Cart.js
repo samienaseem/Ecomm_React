6384468b4bb54126dcb128b1f029084de178833e
Saquib Naseem <samar.naseem.97@gmail.com>
2024-10-14 00:14:37 +0100

    cart screen update

--- frontend/src/Screens/Cart.js
+++ frontend/src/Screens/Cart.js
@@ -1,7 +1,12 @@
+import { useContext } from "react"
+import { Store } from "../Store"
+
 export default function Cart() {
+    const {state,dispatch}=useContext(Store)
+    console.log({'cartScreen': state})
     return(
         <div>
-            <h1>Hii You are in the cart screen</h1>
+            <h1>Hii You are in the cart screen </h1>
         </div>
     )
 }
