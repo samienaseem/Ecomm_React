bcc4d68f655a1ce83e853b2216c72235831936db
Saquib Naseem <samar.naseem.97@gmail.com>
2024-10-17 03:21:59 +0100

    SigninScreen JSX complete

--- frontend/src/Screens/SignInScreen.js
+++ frontend/src/Screens/SignInScreen.js
@@ -2,9 +2,14 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/esm/Button';
 import Container from "react-bootstrap/esm/Container";
 import { Helmet } from "react-helmet-async";
-import { Link, redirect } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export default function SignInScreen(){
+    const location=useLocation()
+    // const redirect = new URLSearchParams(location.search).get('redirect') || '/';
+    const redirectUrl=new URLSearchParams(location.search).get('redirect');
+    const redirect = redirectUrl? redirectUrl : '/'
+
     return (
       <Container className="small-container">
         <Helmet>
@@ -21,15 +26,15 @@ export default function SignInScreen(){
             <Form.Label>Password</Form.Label>
             <Form.Control type="password" required />
           </Form.Group>
-        </Form>
-        <div className="mb-3">
-          <Button type="submit">Sign In</Button>
-        </div>
+          <div className="mb-3">
+            <Button type="submit">Sign In</Button>
+          </div>
 
-        <div className="mb-3">
-          New Customer {' '}
-          <Link to={`/signup?redirect=${redirect}`}>Create Your Account</Link>
-        </div>
+          <div className="mb-3">
+            New Customer?{'  '}
+            <Link to={`/signup?redirect=${redirect}`}>Create Your Account</Link>
+          </div>
+        </Form>
       </Container>
     );
 }
