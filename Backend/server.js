1b621036dcb39d07a83f0661490d5b841e063fa5
Saquib Naseem <samar.naseem.97@gmail.com>
2024-10-17 04:38:05 +0100

    MongoDB configured and connected

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,17 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
 import data from './Data.js';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI).then(()=>{
+  console.log("Connected to MongoDB")
+}).catch(err=>{
+  console.log(err.message)
+})
+
+
 
 const app=express();
 
