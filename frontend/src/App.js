a997f9b3aef87780badfbc481066c42845a65a6c
Saquib Naseem <samar.naseem.97@gmail.com>
2024-10-17 02:51:46 +0100

    SigninScreen Begins

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import './App.css';
 import CartScreen from './Screens/CartScreen';
 import HomeScreen from './Screens/HomeScreen';
 import ProductScreen from './Screens/ProductScreen';
+import SignInScreen from './Screens/SignInScreen';
 import { Store } from './Store';
 
 function App() {
@@ -17,44 +18,44 @@ function App() {
   const {cart}= state;
 
   return (
-      <BrowserRouter>
-        <div className="d-flex flex-column site-container">
-          <header>
-            <Navbar bg="dark" variant="dark">
-              <Container fluid>
-                <LinkContainer to="/">
-                  <Navbar.Brand>11th Street Atelier</Navbar.Brand>
-                </LinkContainer>
-                <Nav className='me-auto'>
-                  <Link to='/cart' className='nav-link'>
-                  Cart {
-                    cart.cartItems.length> 0 && (
-                      <Badge pill bg='danger'>
-                          {cart.cartItems.reduce((a,c)=> a+c.quantity,0)}
-                      </Badge>
-                    )
-                  }
-                  </Link>
-                </Nav>
-              </Container>
-            </Navbar>
-            {/* <a href="/">11th Street Atelier</a> */}
-          </header>
-
-          <main>
+    <BrowserRouter>
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Navbar bg="dark" variant="dark">
             <Container fluid>
-              <Routes>
-                <Route path='/cart' element={<CartScreen/>}/>
-                <Route path="/product/:slug" element={<ProductScreen />} />
-                <Route path="/" element={<HomeScreen />} />
-              </Routes>
+              <LinkContainer to="/">
+                <Navbar.Brand>11th Street Atelier</Navbar.Brand>
+              </LinkContainer>
+              <Nav className="me-auto">
+                <Link to="/cart" className="nav-link">
+                  Cart{' '}
+                  {cart.cartItems.length > 0 && (
+                    <Badge pill bg="danger">
+                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                    </Badge>
+                  )}
+                </Link>
+              </Nav>
             </Container>
-          </main>
-          <footer>
-            <div className="text-center">All Rights Reserved</div>
-          </footer>
-        </div>
-      </BrowserRouter>
+          </Navbar>
+          {/* <a href="/">11th Street Atelier</a> */}
+        </header>
+
+        <main>
+          <Container fluid>
+            <Routes>
+              <Route path="/cart" element={<CartScreen />} />
+              <Route path="/signin" element={<SignInScreen />} />
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">All Rights Reserved</div>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
